11a9dfa372690f27789c8bf229b5fa6ae5ca43bd
Clément <duvivier.clem@gmail.com>
2022-03-15 23:57:06 +0100

    :rocket: composer to managed automatically createAt and updateAt on all entities

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('boochebee@admin.com');
+        $password = $this->hasher->hashPassword($user, 'admin');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        for ($i = 1; $i <= 49; $i++) {
+
+            $user = new User();
+            $user->setEmail('Jobs'.$i.'@test.com');
+            $password = $this->hasher->hashPassword($user, 'user');
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
