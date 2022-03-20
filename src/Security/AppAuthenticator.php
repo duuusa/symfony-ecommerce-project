a02420ecc958f9775a7f7a59c546678198abdd2f
Clément <duvivier.clem@gmail.com>
2022-03-20 17:19:54 +0100

    :bug: redirection fixed

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,7 +50,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('admin'));
+        return new RedirectResponse($this->urlGenerator->generate('app_profile'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
